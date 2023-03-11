9274453ba57ff8199aabff3d799082342af89992
murphy2225 <122998544+murphy2225@users.noreply.github.com>
2023-03-10 22:34:36 -0500

    testing length and width.

--- UnitTest1PM2/UnitTest1PM2.cpp
+++ UnitTest1PM2/UnitTest1PM2.cpp
@@ -4,6 +4,9 @@
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 extern "C" int getPerimeter(int*, int*);
 extern "C" int getArea(int*, int*);
+extern "C" void setLength(int, int*);
+extern "C" void setWidth(int , int*);
+
 namespace UnitTest1PM2
 {
 	TEST_CLASS(UnitTest1PM2)
@@ -26,6 +29,45 @@ namespace UnitTest1PM2
 				int expectedArea = 50;
 				int actualArea = getArea(&length, &width);
 				Assert::AreEqual(expectedArea, actualArea);
+			}
+			TEST_METHOD(checklengthvalid)    // length is between range 
+			{
+				int length = 0;
+				setLength(55, &length);
+				Assert::AreEqual(55, length);
+			}
+			TEST_METHOD(Lengthcheckinvaild)    // length is out of range 
+			{
+				int length = 0;
+				setLength(100, &length);
+				Assert::AreEqual(100, length);
+			}
+			TEST_METHOD(LengthcheckInvalidNegative)    // length is out of range 
+			{
+				int length = 0;
+				setLength(-9, &length);
+				Assert::AreEqual(-9, length);
+			}
+
+			TEST_METHOD(WidthcheckInvalid)    // width is out of range 
+			{
+				int width = 0;
+				setWidth(100, &width);
+				Assert::AreEqual(100, width);
+			}
+
+			TEST_METHOD(Widthcheckvalid)   // width is between range 
+			{
+				int width = 0;
+				setWidth(78, &width);
+				Assert::AreEqual(78, width);
+			}
+
+			TEST_METHOD(WidthcheckInvalidNegative)    // width is out of range
+			{
+				int width = 0;
+				setWidth(-4, &width);
+				Assert::AreEqual(-4, width);
 			}
 		
 	};
