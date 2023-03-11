b57f42a5616ac4aff13c117315183134bf213242
murphy2225 <122998544+murphy2225@users.noreply.github.com>
2023-03-10 22:31:00 -0500

    TESTING PERIMETER AREA.

---
+++ UnitTest1PM/UnitTest1PM.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1PM
+{
+	TEST_CLASS(UnitTest1PM)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
